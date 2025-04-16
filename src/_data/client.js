a064fc417897d8739ebfa8beeb556200f85b225d
Payton Chertude <p.chertude34@gmail.com>
2025-04-16 08:47:41 -0700

    add address

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,15 +1,15 @@
 module.exports = {
-    name: "RV Care Plus",
-    email: "rvcareplus@gmail.com",
-    phoneForTel: "719-373-1090",
-    phoneFormatted: "(719) 373-1090",
-    location: {
-        city: "Colorado Springs",
-        state: "Colorado",
-    },
-    socials: {
-        facebook: "https://www.facebook.com/profile.php?id=100084356192175",
-    },
-    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://rvcareplus.net",
+  name: "RV Care Plus",
+  email: "rvcareplus@gmail.com",
+  phoneForTel: "719-373-1090",
+  phoneFormatted: "(719) 373-1090",
+  address: {
+    city: "Colorado Springs",
+    state: "Colorado",
+  },
+  socials: {
+    facebook: "https://www.facebook.com/profile.php?id=100084356192175",
+  },
+  //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+  domain: "https://rvcareplus.net",
 };
